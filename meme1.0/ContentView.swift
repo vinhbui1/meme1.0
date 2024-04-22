b85214d37e6fdca7af6de1ff9d6aa9ccd0111b63
vinhbui1 <43310798+vinhbui1@users.noreply.github.com>
2024-04-22 14:48:36 +0700

    Initial Commit

---
+++ meme1.0/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  meme1.0
+//
+//  Created by Vinh on 22/04/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ meme1.0/meme1_0App.swift
@@ -0,0 +1,17 @@
+//
+//  meme1_0App.swift
+//  meme1.0
+//
+//  Created by Vinh on 22/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct meme1_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
